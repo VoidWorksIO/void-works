e304bcd8f22301d892de051ce0dba3ace8655f9a
Jens Astrup <jensaiden@gmail.com>
2026-05-28 21:00:30 -0400

    Add shortcut assistant privacy policy

---
+++ src/app/shortcut-assistant/privacy-policy/page.tsx
@@ -0,0 +1,12 @@
+import PrivacyPolicy from '@/app/shortcut-assistant/privacy-policy/privacy-policy'
+
+
+function PrivacyPolicyPage(): React.ReactNode {
+  return (
+    <div className="container mx-auto px-6 sm:py-16">
+      <PrivacyPolicy />
+    </div>
+  )
+}
+
+export default PrivacyPolicyPage
